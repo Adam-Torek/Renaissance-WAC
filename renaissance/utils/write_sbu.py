b9f230e44322c0e22d871086d8dff2d1251c4597
Adam-Torek <adamtorek@u.boisestate.edu>
2026-01-29 12:41:54 -0700

    changed code to preprocess SBU captions dataset

--- renaissance/utils/write_sbu.py
+++ renaissance/utils/write_sbu.py
@@ -4,63 +4,60 @@ import pyarrow as pa
 import gc
 import random
 import os
+import copy
 
 from tqdm import tqdm
 from glob import glob
-
-
-def path2rest(path, iid2captions):
-    split, _, name = path.split("/")[-3:]
-    split = split.split("_")[-1]
-    iid = name
-
-    with open(path, "rb") as fp:
-        binary = fp.read()
-
-    captions = iid2captions[iid]
-
-    return [
-        binary,
-        captions,
-        iid,
-        split,
-    ]
-
+from PIL import Image
 
 def write_sbu(root, dataset_root):
-    with open(f"{root}/annot.json", "r") as fp:
-        captions = json.load(fp)
+    captions_list = []
+    image_paths_list = []
+    for root, folders, _ in os.walk(root):
+        for folder in folders:
+                for file in os.listdir(os.path.join(root, folder)):   
+                    if ".txt" in file:
+                        captions_list.append(os.path.join(root, folder, file))
+                    if ".jpg" in file:
+                        image_paths_list.append(os.path.join(root, folder, file))
+    
+    captions_list.sort()
+    image_paths_list.sort()
 
-    iid2captions = dict()
-    for cap in tqdm(captions):
-        iid = cap[0].split("/")[-1]
-        iid2captions[iid] = [cap[1]]
+    column_values = []
+    for i, image_path in enumerate(image_paths_list):
+        caption = captions_list[i]
+        image = open(image_path, "rb").read()
+        column_values.append([caption, image, i])
 
-    paths = list(glob(f"{root}/images_train/*/*"))
-    random.shuffle(paths)
-    caption_paths = [path for path in paths if path.split("/")[-1] in iid2captions]
-    if len(paths) == len(caption_paths):
-        print("all images have caption annotations")
-    else:
-        print("not all images have caption annotations")
-    print(
-        len(paths), len(caption_paths), len(iid2captions),
-    )
+    del image_paths_list, captions_list
 
-    sub_len = int(len(caption_paths) // 100000)
-    subs = list(range(sub_len + 1))
-    for sub in subs:
-        sub_paths = caption_paths[sub * 100000 : (sub + 1) * 100000]
-        bs = [path2rest(path, iid2captions) for path in tqdm(sub_paths)]
-        dataframe = pd.DataFrame(bs, columns=["image", "caption", "image_id", "split"],)
+    sbu_dataframe = pd.DataFrame(column_values, columns=["caption","image","image_id"])
+    train_dataframe = sbu_dataframe.sample(frac=0.8)
+    val_test_dataframe = copy.deepcopy(sbu_dataframe[~sbu_dataframe["image_id"].isin(train_dataframe["image_id"])])
 
-        table = pa.Table.from_pandas(dataframe)
+    del sbu_dataframe
 
-        os.makedirs(dataset_root, exist_ok=True)
-        with pa.OSFile(f"{dataset_root}/sbu_{sub}.arrow", "wb") as sink:
-            with pa.RecordBatchFileWriter(sink, table.schema) as writer:
-                writer.write_table(table)
-        del dataframe
-        del table
-        del bs
-        gc.collect()
+    test_dataframe = val_test_dataframe.sample(frac=0.5)
+    val_dataframe = copy.deepcopy(val_test_dataframe[~val_test_dataframe["image_id"].isin(test_dataframe["image_id"])])
+
+    train_dataframe["split"] = "train"
+    val_dataframe["split"] = "val"
+    test_dataframe["split"] = "test"
+
+    table_dict = {
+         "train": pa.Table.from_pandas(train_dataframe),
+         "val": pa.Table.from_pandas(val_dataframe),
+         "test": pa.Table.from_pandas(test_dataframe),
+    }
+
+    del train_dataframe, val_dataframe, test_dataframe
+
+    os.makedirs(dataset_root, exist_ok=True)
+
+    for subsplit, table in table_dict.items():
+        with pa.OSFile(f"{dataset_root}/sbu_{subsplit}.arrow", "wb") as sink:
+            with pa.RecordBatchFileWriter(sink, table.schema) as pa_file:
+                pa_file.write_table(table)
+    
+    gc.collect()
