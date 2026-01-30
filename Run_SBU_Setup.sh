2c8dbfa7a3f125a567e3dba0e206e8ea2cba6094
Adam-Torek <adamtorek@u.boisestate.edu>
2026-01-29 23:40:55 -0700

    added script to submit SBU captions job to CPU node on Borah

---
+++ Run_SBU_Setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH -J SBU_Dataset_Setup         # job name
+#SBATCH -o slurm_logs/renaissance_wac/sbu_setup.o%j               # output and error file name (%j expands to jobID)  
+#SBATCH --nodes=1			               # Number of nodes to run on
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=48      # Number of CPU nodes per task to run
+#SBATCH -p cpu                    # queue (partition)
+#SBATCH -t 24:00:00                    # run time (hh:mm:ss)
+
+source activate renaissance
+
+srun python3 renaissance/utils/run_write.py --dataset_to_write sbu --dataset_source_path data/sbucaptions --dataset_destination_path data/arrow/sbucaptions
