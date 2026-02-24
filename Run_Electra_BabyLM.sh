d3fc8fc0b4f0d4222272b35eba617596f75a709d
Adam-Torek <adamtorek@u.boisestate.edu>
2026-02-23 17:11:31 -0700

    lowered number of tasks per node in babyLM electra config

--- Run_Electra_BabyLM.sh
+++ Run_Electra_BabyLM.sh
@@ -4,7 +4,7 @@
 #SBATCH -o slurm_logs/renaissance_wac/renaissance_electra_babylm.o%j               # output and error file name (%j expands to jobID)  
 #SBATCH --nodes=1			               # Number of nodes to run on
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=48      # Number of CPU nodes per task to run
+#SBATCH --cpus-per-task=16      # Number of CPU nodes per task to run
 #SBATCH --gres=gpu:L40:1
 #SBATCH -p gpu-l40                     # queue (partition)
 #SBATCH -t 48:00:00                    # run time (hh:mm:ss)
