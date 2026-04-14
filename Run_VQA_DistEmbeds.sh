d0216900a0bcda92f6f948ebec94319c029b4496
Adam-Torek <adamtorek@u.boisestate.edu>
2026-04-14 17:50:06 -0600

    edited VQA runner scripts to use 2 GPUs and edited distribution scripts to run job arrays for each distribution combo

--- Run_VQA_DistEmbeds.sh
+++ Run_VQA_DistEmbeds.sh
@@ -3,11 +3,12 @@
 #SBATCH -J WAC_VQA_EmbedDists         # job name
 #SBATCH -o slurm_logs/renaissance_wac/wac_vqa_embeddists.o%j               # output and error file name (%j expands to jobID)  
 #SBATCH --nodes=1			               # Number of nodes to run on
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=16      # Number of CPU nodes per task to run
-#SBATCH --gres=gpu:L40:1
-#SBATCH -p gpu-l40                     # queue (partition)
+#SBATCH --gres=gpu:L40:2
+#SBATCH -p shortgpu-l40                     # queue (partition)
 #SBATCH -t 128:00:00                    # run time (hh:mm:ss)
+#SBATCH --array=1-3
 
 source activate renaissance
 
@@ -16,9 +17,10 @@ wac_distribution_matrices=(key query value)
 for wac_distribution_matrix in "${wac_distribution_matrices[@]}"
 do
 
-    srun python3 run.py with pretrain_wac_enabled_vqa_twotower_electrasmall_deit_small \
+    python3 run.py with pretrain_wac_enabled_vqa_twotower_electrasmall_deit_small \
                                 exp_name=pretrain_wac_enabled_$wac_distribution_matrix\_embeddings_vqa_twotower_electrasmall_deit_small \
                                 wac_distribution_matrix=$wac_distribution_matrix \
-                                use_wac_embeddings=True
+                                use_wac_embeddings=True \
+                                num_gpus=2
 
 done
--- Run_VQA_Embeds.sh
+++ Run_VQA_Embeds.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH -J WAC_VQA_Embeds         # job name
-#SBATCH -o slurm_logs/renaissance_wac/wac_vqa_Embeds.o%j               # output and error file name (%j expands to jobID)  
+#SBATCH -J WAC_MultiGPU_VQA_Test         # job name
+#SBATCH -o slurm_logs/renaissance_wac/wac_multigpu_vqa_test.o%j               # output and error file name (%j expands to jobID)  
 #SBATCH --nodes=1			               # Number of nodes to run on
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=16      # Number of CPU nodes per task to run
-#SBATCH --gres=gpu:L40:1
-#SBATCH -p gpu-l40                     # queue (partition)
+#SBATCH --gres=gpu:L40:2
+#SBATCH -p shortgpu-l40                     # queue (partition)
 #SBATCH -t 128:00:00                    # run time (hh:mm:ss)
 
 source activate renaissance
@@ -14,5 +14,5 @@ source activate renaissance
 srun python3 run.py with pretrain_wac_enabled_vqa_twotower_electrasmall_deit_small \
                                     exp_name=pretrain_wac_enabled_embeddings_vqa_twotower_electrasmall_deit_small \
                                     wac_distribution_matrix=None \
-                                    use_wac_embeddings=True
-
+                                    use_wac_embeddings=True \
+                                    num_gpus=2
