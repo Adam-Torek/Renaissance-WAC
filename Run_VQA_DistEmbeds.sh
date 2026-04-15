e31796a71547184fc5c5f459e178e2edfc772207
Adam-Torek <adamtorek@u.boisestate.edu>
2026-04-15 11:29:24 -0600

    removed job array for dists and distembeds and changed number of GPUs to 4 for training and evaluation

--- Run_VQA_DistEmbeds.sh
+++ Run_VQA_DistEmbeds.sh
@@ -3,12 +3,11 @@
 #SBATCH -J WAC_VQA_EmbedDists         # job name
 #SBATCH -o slurm_logs/renaissance_wac/wac_vqa_embeddists.o%j               # output and error file name (%j expands to jobID)  
 #SBATCH --nodes=1			               # Number of nodes to run on
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=16      # Number of CPU nodes per task to run
-#SBATCH --gres=gpu:L40:2
+#SBATCH --gres=gpu:L40:4
 #SBATCH -p shortgpu-l40                     # queue (partition)
 #SBATCH -t 128:00:00                    # run time (hh:mm:ss)
-#SBATCH --array=1-3
 
 source activate renaissance
 
@@ -17,10 +16,10 @@ wac_distribution_matrices=(key query value)
 for wac_distribution_matrix in "${wac_distribution_matrices[@]}"
 do
 
-    python3 run.py with pretrain_wac_enabled_vqa_twotower_electrasmall_deit_small \
+    srun python3 run.py with pretrain_wac_enabled_vqa_twotower_electrasmall_deit_small \
                                 exp_name=pretrain_wac_enabled_$wac_distribution_matrix\_embeddings_vqa_twotower_electrasmall_deit_small \
                                 wac_distribution_matrix=$wac_distribution_matrix \
                                 use_wac_embeddings=True \
-                                num_gpus=2
+                                num_gpus=4
 
 done
